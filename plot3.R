335261b14481fecf379bf8f8c593db49bb1ffca4
Liang Dong <vc2004@gmail.com>
2014-08-09 15:06:04 +0800

    add comments and modify plot4.R

--- plot3.R
+++ plot3.R
@@ -1,12 +1,20 @@
+# Loading the data into R and subset with specific date
+
 test <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 test.sub <- test[test$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+# Convert the factor into number or character
 
 test.sub[,1] <- as.character(test.sub[,1])
 cols = c(3, 4, 5, 6, 7, 8, 9)
 test.sub[,cols] = apply(test.sub[,cols], 2, function(x) as.numeric(as.character(x)))
 
+# Combine the time column into standard time format
+
 test.sub$Time <- strptime(paste(test.sub$Date, test.sub$Time), "%d/%m/%Y %H:%M:%S")
 test.sub <- test.sub[,2:9]
+
+# Draw the PNG
 
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
 plot(test.sub$Time, test.sub$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
--- plot4.R
+++ plot4.R
@@ -1,16 +1,24 @@
+# Loading the data into R and subset with specific date
+
 test <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
 test.sub <- test[test$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+# Convert the factor into number or character
 
 test.sub[,1] <- as.character(test.sub[,1])
 cols = c(3, 4, 5, 6, 7, 8, 9)
 test.sub[,cols] = apply(test.sub[,cols], 2, function(x) as.numeric(as.character(x)))
 
+# Combine the time column into standard time format
+
 test.sub$Time <- strptime(paste(test.sub$Date, test.sub$Time), "%d/%m/%Y %H:%M:%S")
 test.sub <- test.sub[,2:9]
 
+# Draw the PNG
+
 png(filename = "plot4.png", width = 480, height = 480, units = "px")
 par(mfrow = c(2, 2))
-plot(test.sub$Time, test.sub$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(test.sub$Time, test.sub$Global_active_power, type="l", xlab="", ylab="Global Active Power")
 plot(test.sub$Time, test.sub$Voltage, type="l", xlab="datetime", ylab="Voltage")
 plot(test.sub$Time, test.sub$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(test.sub$Time, test.sub$Sub_metering_2, col = "red")
