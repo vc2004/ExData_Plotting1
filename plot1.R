f50cd1d1aeeb7b76d9aa3205f77a337be0c77f2e
Liang Dong <vc2004@gmail.com>
2014-08-09 14:49:31 +0800

    first commit

---
+++ plot1.R
@@ -0,0 +1,13 @@
+test <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+test.sub <- test[test$Date %in% c("1/2/2007", "2/2/2007"), ]
+
+test.sub[,1] <- as.character(test.sub[,1])
+cols = c(3, 4, 5, 6, 7, 8, 9)
+test.sub[,cols] = apply(test.sub[,cols], 2, function(x) as.numeric(as.character(x)))
+
+test.sub$Time <- strptime(paste(test.sub$Date, test.sub$Time), "%d/%m/%Y %H:%M:%S")
+test.sub <- test.sub[,2:9]
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(test.sub$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
+dev.off()
